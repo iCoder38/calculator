d71ea44902512f43047ee341972588cf903fb6bb
iCoder38 <dishant.rajput@evirtualservices.com>
2025-02-18 19:24:07 +0530

    Saved scanner

--- lib/Classes/Screens/Maths/maths.dart
+++ lib/Classes/Screens/Maths/maths.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:calculator/Classes/Utils/drawer.dart';
 import 'package:calculator/Classes/Utils/resources.dart';
+import 'package:calculator/Classes/Utils/reusable/resuable.dart';
 import 'package:calculator/Classes/Utils/utils.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MathsScreen extends StatefulWidget {
   const MathsScreen({super.key});
@@ -14,6 +19,9 @@ class _MathsScreenState extends State<MathsScreen> {
   // scaffold
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  // file
+  File? _image;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +29,12 @@ class _MathsScreenState extends State<MathsScreen> {
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: customText(AppText().kScanQuestion, 16.0, context),
+        title: customText(
+          AppText().kScanQuestion,
+          16.0,
+          context,
+          lightModeColor: AppColor().kWhite,
+        ),
         centerTitle: true,
         automaticallyImplyLeading: false,
         leading: IconButton(
@@ -32,7 +45,85 @@ class _MathsScreenState extends State<MathsScreen> {
         ),
       ),
       drawer: CustomDrawer(),
-      // body: _UIKit(context),
+      body: _UIKit(context),
     );
+  }
+
+  Widget _UIKit(context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CustomBannerButton(
+            text: '',
+            height: 280,
+            textSize: 16.0,
+            bgColor: Colors.transparent,
+            bgImage: AppImage().kPrimaryBlueImage,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child:
+                  _image == null
+                      ? Image.asset(AppImage().kAppLogo, fit: BoxFit.contain)
+                      : Image.file(
+                        _image!,
+                        width: 200,
+                        height: 200,
+                        fit: BoxFit.contain,
+                      ),
+            ),
+          ),
+        ),
+
+        Spacer(),
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CustomBannerButton(
+                  text: 'Gallery',
+                  textSize: 16.0,
+                  bgColor: Colors.blue,
+                  bgImage: AppImage().kPrimaryBlueImage,
+                  onPressed: () {
+                    _pickImage(context);
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CustomBannerButton(
+                  text: 'Camera',
+                  textSize: 16.0,
+                  bgColor: Colors.blue,
+                  bgImage: AppImage().kPrimaryYellowImage,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Future<void> _pickImage(BuildContext context) async {
+    final ImagePicker picker = ImagePicker();
+    // Pick an image from the gallery
+    final XFile? pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path); // Store the selected image in _image
+      });
+    } else {
+      if (kDebugMode) {
+        print('No image selected.');
+      }
+    }
   }
 }
--- lib/Classes/Utils/reusable/resuable.dart
+++ lib/Classes/Utils/reusable/resuable.dart
@@ -7,6 +7,9 @@ class CustomBannerButton extends StatelessWidget {
   final String? iconImage; // Optional icon
   final Color bgColor;
   final String? bgImage; // Optional background image
+  final double? height; // Optional height, default is 70
+  final Widget? child; // Optional child widget
+  final VoidCallback? onPressed; // Optional onPressed callback
 
   const CustomBannerButton({
     super.key,
@@ -15,41 +18,53 @@ class CustomBannerButton extends StatelessWidget {
     this.iconImage, // Optional
     required this.bgColor,
     this.bgImage, // Optional
+    this.height, // Optional height, default to 70 if not provided
+    this.child, // Optional child widget
+    this.onPressed, // Optional onPressed callback
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 70,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: bgColor,
-        borderRadius: BorderRadius.circular(12),
-        image:
-            bgImage != null
-                ? DecorationImage(
-                  image: ExactAssetImage(bgImage!),
-                  fit: BoxFit.cover,
-                )
-                : null,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (iconImage != null) // Show icon only if provided
+    return GestureDetector(
+      onTap: onPressed, // Trigger onPressed if provided
+      child: Container(
+        height: height ?? 70, // Use the provided height or default to 70
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(12),
+          image:
+              bgImage != null
+                  ? DecorationImage(
+                    image: ExactAssetImage(bgImage!),
+                    fit: BoxFit.cover,
+                  )
+                  : null,
+        ),
+        child:
+            child ?? // If child is provided, use it, otherwise use the default layout
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(width: 28, height: 28, child: Image.asset(iconImage!)),
-                SizedBox(width: 4), // Space between icon and text
+                if (iconImage != null) // Show icon only if provided
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 28,
+                        height: 28,
+                        child: Image.asset(iconImage!),
+                      ),
+                      SizedBox(width: 4), // Space between icon and text
+                    ],
+                  ),
+                customText(
+                  text.toUpperCase(),
+                  textSize, // Dynamic text size
+                  context,
+                  fontWeight: FontWeight.w800,
+                ),
               ],
             ),
-          customText(
-            text.toUpperCase(),
-            textSize, // Dynamic text size
-            context,
-            fontWeight: FontWeight.w800,
-          ),
-        ],
       ),
     );
   }
